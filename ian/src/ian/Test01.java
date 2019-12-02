1ca9c1b17fbf83960286ed4af2be728c05921019
Tarena <Tarena@DESKTOP-DQR1NLH>
2019-12-02 15:53:54 +0800

    first

---
+++ ian/src/ian/Test01.java
@@ -0,0 +1,13 @@
+package ian;
+
+public class Test01 {
+	public static void main(String[] args) {
+		int i = 0;
+		int sum = 0;
+		while(i<100) {
+			i++;
+			sum+=i;
+		}
+		System.out.println(sum);
+	}
+}
